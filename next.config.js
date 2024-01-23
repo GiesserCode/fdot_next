391eea6ce3eee5dce46891f6f0c6d193965918b4
GiesserCode <floriangiessercode@outlook.com>
2024-01-23 19:51:05 +0100

    new

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    // Other configurations...
-
-    // Add this line for static HTML export
-    exportPathMap: function () {
-        return {
-            '/': { page: '/' },
-            // Add other pages as needed
-        };
-    },
-};
-
+module.exports = nextConfig
