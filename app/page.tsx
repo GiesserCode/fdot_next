6f96ede918ef17ca6737016dbeb1614175057af0
GiesserCode <floriangiessercode@outlook.com>
2024-02-28 21:57:09 +0100

    metadata titlte description

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import {Metadata} from "next";
 import Clientpage from "@/app/components/Clientpage";
 
 export const metadata: Metadata = {
-    title: 'Fdot',
-    description: "Auf der Suche nach einer Neuen personalisierten Webseite? Ganzer Service von Design bis zum Aufschalten Ihrer neuen Webseite.",
+    title: 'Fdot - Erstellen Sie Ihre persönliche Website',
+    description: "Auf der Suche nach einer Neuen personalisierten Webseite? Ganzer Service von Design bis zum Aufschalten Ihrer neuen Webseite. Kontaktieren Sie mich noch heute, um Ihre Träume zu verwirklichen und Ihre Website zu erstellen.",
     metadataBase: new URL('https://fdot.ch'),
     authors: [
         {name: "Florian Giesser", url: "https://fdot.ch"}
