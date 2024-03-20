4d629d4d495f1710a20e98b6294053f996f1b40d
GiesserCode <floriangiessercode@outlook.com>
2024-03-20 15:06:02 +0100

    new

--- app/components/dashboard/UserTasks.tsx
+++ app/components/dashboard/UserTasks.tsx
@@ -28,15 +28,15 @@ function Tasks({users}: any) {
         {users.tasks.map((item: any, index: number) => (
             <div key={index} className={`w-full bg-normalBG rounded-xl p-5 text-xl flex`}
                  onClick={() => toggleTextarea(index)}>
-                <div className={`w-full flex items-center flex-wrap gap-5`}>
-                    <div className={`w-1/5 max-lg:w-min whitespace-nowrap ${blackOpsOne.className} antialiased`}>{item.name}</div>
-                    <div className={`w-1/5 max-lg:w-min whitespace-nowrap`}>
+                <div className={`w-full flex items-center flex-wrap max-lg:gap-5`}>
+                    <div className={`w-1/5 max-lg:w-full whitespace-nowrap ${blackOpsOne.className} antialiased`}>{item.name}</div>
+                    <div className={`w-1/5 max-lg:min-w-1/3 max-lg:w-min text-sm whitespace-nowrap`}>
                         <div
                             className={`${item.status === "nicht begonnen" ? "bg-lightBG" : item.status === "begonnen" ? "bg-[#30436b]" : "bg-[#375840]"} rounded-lg p-2 w-min whitespace-nowrap`}>
                             {item.status}
                         </div>
                     </div>
-                    <div className={`w-1/5 max-lg:w-min whitespace-nowrap`}>
+                    <div className={`w-1/5 max-lg:min-w-1/3 max-lg:w-min text-sm whitespace-nowrap`}>
                         <div className={`${item.type === "Design" ? "bg-[#462f63]" : "bg-[#57242c]"} w-min p-2 rounded-lg`}>
                             {item.type}
                         </div>
@@ -45,7 +45,7 @@ function Tasks({users}: any) {
                     <div className={`w-1/5 max-lg:w-min whitespace-nowrap`}>{item.endDate}</div>
                 </div>
                 <div
-                    className={`absolute right-0 border border-lightBG h-full max-w-[500px] break-words top-0 bg-normalLightBg rounded-xl p-2 transition duration-500 ease-in-out ${visibleTextarea[index] ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
+                    className={`absolute right-0 border border-lightBG h-full max-w-[500px] break-words top-0 bg-normalLightBg rounded-xl p-2 transition duration-500 max-lg:fixed max-lg:w-10/12 max-lg:mx-[calc(100%/12)] max-lg:mt-6 max-lg:h-min ease-in-out ${visibleTextarea[index] ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
                     {item.notes}
                 </div>
             </div>
