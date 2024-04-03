497038f8c835c84a1d9cf33ffd17c3e5855a0757
GiesserCode <floriangiessercode@outlook.com>
2024-04-03 21:26:11 +0200

    vali secured

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -6,7 +6,7 @@ import {BackSVG} from "@/app/ui/SVG";
 const Learnlearn = async ({searchParams,}: { searchParams?: { query?: string; }; }) => {
     const accounts = await getUserData()
     const wordsets_user = await getWordsetsData(accounts)
-    const query = searchParams?.query || false;
+    const query = searchParams?.query?.length === 36 ? searchParams.query : false;
     const wordsetData = query ? await getWordsetbyId(query) : wordsets_user[0]
     const userWordset = accounts![0].wordsets_user.find((item: any) => item.id === wordsetData[0].id);
 
