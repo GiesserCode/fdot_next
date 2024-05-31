2e9db5f05723a38bf93cbdd1004d8faf5a85f4cd
GiesserCode <floriangiessercode@outlook.com>
2024-05-31 16:33:52 +0200

    no pricing

--- app/components/Pricing.tsx
+++ app/components/Pricing.tsx
@@ -36,11 +36,10 @@ function Simple({item, index} : any) {
         <div className="w-full h-min grid place-items-center my-8 bottom">
             <div className={`maxW rounded-2xl p-[2px] ${index === 0 ? "background-gradient" : "bg-lightBG"}  w-10/12`}>
                 <div className="flex max-lg:items-center max-lg:flex-col overflow-hidden bg-darkBg rounded-2xl">
-                    <div className="text-primary p-4 h-1/2 w-9/12 flex flex-col gap-4 max-lg:w-full">
+                    <div className="text-primary p-4 h-1/2 w-full flex flex-col gap-4 max-lg:w-full">
                         <SimpleTitle item={item}/>
                         <SimpleIncluded item={item} index={index}/>
                     </div>
-                    <SimplePrice item={item}/>
                 </div>
             </div>
         </div>
