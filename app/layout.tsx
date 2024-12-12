6f69984a0c1ec79c72973b8b0b09ed2f2e073d47
GiesserCode <floriangiessercode@outlook.com>
2024-12-12 12:06:52 +0100

    meta change

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,13 @@ export const metadata: Metadata = {
   title: "Fdot lassen Sie sich Ihre persönliche Webseite erstellen",
   description:
     "Ihre Webseite, gestaltet nach ihren Wünschen. Lassen Sie uns gemeinsam Ihre Vorstellungen in die Realität umsetzen. Ihre digitale Identität, auf dem neusten Stand der Technologie. Hinterlassen Sie so einen bleibenden Eindruck.",
+  openGraph: {
+    title: "Fdot lassen Sie sich Ihre persönliche Webseite erstellen",
+    description:
+      "Ihre Webseite, gestaltet nach ihren Wünschen. Lassen Sie uns gemeinsam Ihre Vorstellungen in die Realität umsetzen. Ihre digitale Identität, auf dem neusten Stand der Technologie. Hinterlassen Sie so einen bleibenden Eindruck.",
+    url: "https://fdot.ch",
+    siteName: "Fdot",
+  },
 };
 
 export default function RootLayout({
