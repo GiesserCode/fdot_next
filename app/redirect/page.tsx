4ed4fea3631140fb754fe8ffb45168cb233d6a18
GiesserCode <floriangiessercode@outlook.com>
2025-07-28 19:23:25 +0200

    fix

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,12 +1,19 @@
+"use client"; // Add this if using app directory
+
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation"; // use this in app directory
 
 export default function RedirectPage() {
   const router = useRouter();
 
   useEffect(() => {
     router.push("https://infofdot.wixstudio.com/celine-50");
-  }, []);
+  }, [router]);
 
-  return null;
+  return (
+    <section className="section-Center h-screen">
+      <a className="text-white">Redirecting... </a>
+      <a>Please wait a moment.</a>
+    </section>
+  );
 }
