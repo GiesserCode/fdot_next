1523df846dcb2fd8620f42df105963feae4c876f
GiesserCode <floriangiessercode@outlook.com>
2025-07-28 21:21:48 +0200

    new commit animation in redirect

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,19 +1,26 @@
 "use client"; // Add this if using app directory
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation"; // use this in app directory
 
 export default function RedirectPage() {
   const router = useRouter();
+  const [dots, setDots] = useState("");
 
   useEffect(() => {
     router.push("https://infofdot.wixstudio.com/celine-50");
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + "." : ""));
+    }, 100);
+    return () => clearInterval(interval);
   }, [router]);
 
   return (
     <section className="section-Center h-screen">
-      <a className="text-white">Redirecting... </a>
-      <a>Please wait a moment.</a>
+      <div className="flex gap-5">
+        <a className="text-white">Redirecting{dots}</a>
+        <a>Please wait a moment.</a>
+      </div>
     </section>
   );
 }
